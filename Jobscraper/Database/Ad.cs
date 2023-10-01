fd428bc721eff317bd32dcfd023be65eccbac1cf
LukeLinkwalker <mikkelbjerg95@gmail.com>
2023-10-01 20:52:53 +0200

    Added DAO classes for interacting with database

---
+++ Jobscraper/Database/Ad.cs
@@ -0,0 +1,41 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobscraper.Database
+{
+    public class Ad
+    {
+        [PrimaryKey, AutoIncrement, NotNull]
+        public int ID { get; set; }
+
+        [NotNull]
+        public string Title { get; set; }
+
+        [NotNull]
+        public string URL { get; set; }
+
+        [NotNull]
+        public string Timestamp { get; set; }
+
+        [OneToMany]
+        public List<string> Keywords { get; set; }
+
+        public Ad()
+        {
+            this.Title = string.Empty;
+            this.URL = string.Empty;
+            this.Timestamp = string.Empty;
+            this.Keywords = new List<string>();
+        }
+
+        public DateTime getTimestamp()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ Jobscraper/Database/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Jobscraper.Database
+{
+    public class Database
+    {
+        public const SQLiteConnection connection = null;
+
+        public Database()
+        {
+            SQLiteConnection _connection = new SQLiteConnection("data.db");
+            _connection.CreateTable<Scraper>();
+            _connection.CreateTable<Ad>();
+        }
+    }
+}
