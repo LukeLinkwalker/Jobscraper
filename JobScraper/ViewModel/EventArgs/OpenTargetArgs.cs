62c16a601e0e0eb51b37eeb2cac6c4766761d857
LukeLinkwalker <mikkelbjerg95@gmail.com>
2023-10-04 18:10:05 +0200

    Added OpenTargetArgs.. Forgot

---
+++ JobScraper/ViewModel/EventArgs/OpenTargetArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobScraper.ViewModel.EventArgs
+{
+    public class OpenTargetArgs
+    {
+        public string URL { get; set; }
+    }
+}
