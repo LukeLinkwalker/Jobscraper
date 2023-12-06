6d91889c4349b61919e5a7792d030eb8e915da26
LukeLinkwalker <mikkelbjerg95@gmail.com>
2023-12-06 22:14:11 +0100

    Added keyword class for handling more types of keywords

---
+++ JobScraper/Model/Filter/Keyword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobScraper.Model.Filter
+{
+    public class Keyword
+    {
+        public string text { get; set; }
+        public Type type { get; set; }
+
+        public enum Type
+        {
+            Must = 0,
+            Should = 1,
+            Cannot = 2
+        }
+    }
+}
